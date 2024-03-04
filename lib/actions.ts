d8a2e3ac040ede8620e74b38d4eea9f7e94b4f6c
Rifat Khan <rifat6633@gmail.com>
2024-03-04 19:40:51 +0600

    update: saveContent operation to upsert

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,19 +2,25 @@
 
 import prisma from "@/lib/prismadb";
 
-type Content = {
-  id: string;
-  type: string;
-  children: {
-    text: string;
-  }[];
-}[];
+type Content =
+  | {
+      id: string;
+      type: string;
+      children: {
+        text: string;
+      }[];
+    }[]
+  | null;
 
 export async function saveContent(content: Content) {
   try {
     const constStr = JSON.stringify(content);
 
-    await prisma.content.create({ data: { content: constStr } });
+    await prisma.content.upsert({
+      create: { id: "1", content: constStr },
+      update: { content: "constStr" },
+      where: { id: "1" },
+    });
   } catch (e) {
     console.log(e);
     throw new Error("Failed to save.");
