20780ba47c527835f786384fdff96103092d21c8
Rifat Khan <rifat6633@gmail.com>
2024-03-04 22:46:17 +0600

    design: Content page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,23 @@
-export default function Pome() {
-  return <main className="container">hello</main>;
+import EditorContent from "@/components/editor-content";
+import { Button } from "@/components/plate-ui/button";
+import { getContent } from "@/lib/actions";
+import Link from "next/link";
+
+export default async function Page() {
+  const content = await getContent();
+
+  return (
+    <main className="container">
+      {content.length !== 0 ? (
+        <EditorContent content={content} />
+      ) : (
+        <section className="py-10 grid place-content-center gap-2">
+          <h1 className="text-muted-foreground">No Content Found</h1>
+          <Button variant={"outline"} asChild>
+            <Link href={"/editor"}>Write Content</Link>
+          </Button>
+        </section>
+      )}
+    </main>
+  );
 }
