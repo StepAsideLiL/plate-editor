ebbaace5d5a36c043c638231abb938d4132c83b1
Rifat Khan <rifat6633@gmail.com>
2024-03-04 22:49:33 +0600

    create: ReadOnlyContent for showing content on content page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import EditorContent from "@/components/editor-content";
+import ReadOnlyContent from "@/components/readonly-content";
 import { Button } from "@/components/plate-ui/button";
 import { getContent } from "@/lib/actions";
 import Link from "next/link";
@@ -9,7 +9,7 @@ export default async function Page() {
   return (
     <main className="container">
       {content.length !== 0 ? (
-        <EditorContent content={content} />
+        <ReadOnlyContent content={content} />
       ) : (
         <section className="py-10 grid place-content-center gap-2">
           <h1 className="text-muted-foreground">No Content Found</h1>
---
+++ components/readonly-content.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Content } from "@/lib/actions";
+import { Plate } from "@udecode/plate-common";
+import { Editor } from "./plate-ui/editor";
+
+export default function ReadOnlyContent({ content }: { content: Content }) {
+  return (
+    <section>
+      {content && (
+        <Plate value={content} readOnly>
+          <Editor />
+        </Plate>
+      )}
+    </section>
+  );
+}
