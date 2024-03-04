a71242bc158ed34b4db061dd52a631a49608fdcd
Rifat Khan <rifat6633@gmail.com>
2024-03-04 22:51:37 +0600

    update: Editor import path

--- components/readonly-content.tsx
+++ components/readonly-content.tsx
@@ -2,7 +2,7 @@
 
 import { Content } from "@/lib/actions";
 import { Plate } from "@udecode/plate-common";
-import { Editor } from "./plate-ui/editor";
+import { Editor } from "@/components/plate-ui/editor";
 
 export default function ReadOnlyContent({ content }: { content: Content }) {
   return (
