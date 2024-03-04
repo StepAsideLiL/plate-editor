cd914ba7446968ffbc9bdd8ccda0c41f647eaf65
Rifat Khan <rifat6633@gmail.com>
2024-03-04 23:06:46 +0600

    import: plugins from plugins file

--- components/readonly-content.tsx
+++ components/readonly-content.tsx
@@ -3,12 +3,13 @@
 import { Content } from "@/lib/actions";
 import { Plate } from "@udecode/plate-common";
 import { Editor } from "@/components/plate-ui/editor";
+import { plugins } from "@/components/plugins";
 
 export default function ReadOnlyContent({ content }: { content: Content }) {
   return (
     <section>
       {content && (
-        <Plate value={content} readOnly>
+        <Plate plugins={plugins} value={content} readOnly>
           <Editor className="border-none" />
         </Plate>
       )}
