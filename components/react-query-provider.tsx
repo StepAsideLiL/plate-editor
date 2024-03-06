c41859c76608410adac6704b05d19258e4e04fe3
Rifat Khan <rifat6633@gmail.com>
2024-03-06 10:01:31 +0600

    fix: turn ReactQueryProvider to client component

--- components/react-query-provider.tsx
+++ components/react-query-provider.tsx
@@ -1,6 +1,7 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Create a client
 const queryClient = new QueryClient();
 
 export default function ReactQueryProvider({
