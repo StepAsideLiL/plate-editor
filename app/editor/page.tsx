4451b59b4978649012fed409f30ea34a6e58bbd0
Rifat Khan <rifat6633@gmail.com>
2024-03-06 09:02:23 +0600

    update: pass content id to getContent function

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -2,7 +2,7 @@ import PlateEditor from "@/components/plate-editor";
 import { getContent } from "@/lib/actions";
 
 export default async function Page() {
-  const content = await getContent();
+  const content = await getContent("1");
 
   const initialValue =
     content.length !== 0
