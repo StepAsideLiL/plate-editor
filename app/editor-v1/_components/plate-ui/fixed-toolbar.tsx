be9c11dc3d2517499d94897c2f63f3002b75dd20
Rifat Khan <rifat6633@gmail.com>
2024-03-16 14:20:41 +0600

    add: sticky position

--- app/editor-v1/_components/plate-ui/fixed-toolbar.tsx
+++ app/editor-v1/_components/plate-ui/fixed-toolbar.tsx
@@ -2,4 +2,7 @@ import { withCn } from "@udecode/cn";
 
 import { Toolbar } from "./toolbar";
 
-export const FixedToolbar = withCn(Toolbar);
+export const FixedToolbar = withCn(
+  Toolbar,
+  "sticky left-0 top-[57px] z-50 bg-transparent"
+);
